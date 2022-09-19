eae2019a920fd5388b6f212df75bd255a9ee1278
Felipe Guilherme Silva <hyperdarks693@outlook.com>
2022-09-19 15:10:36 -0300

    Horario

---
+++ ff/src/ff/Horario.java
@@ -0,0 +1,28 @@
+package ff;
+
+import java.util.Scanner;
+
+public class Horario {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite os segundos");
+		int horario = sc.nextInt();
+		int resto = horario % 3600;
+		
+		int hora = horario / 3600 ;
+		
+		
+		
+		int minuto = resto / 60;
+		
+		int segundo = resto % 60;
+		
+		
+		
+		System.out.println(hora+ ":"+minuto+":"+segundo);
+		
+
+	}
+
+}
