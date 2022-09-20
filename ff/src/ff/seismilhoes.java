f91b21366a3fc5d30b8929dfd8bfddb2764f2cd7
Felipe Guilherme Silva <hyperdarks693@outlook.com>
2022-09-20 20:57:02 -0300

    seismilhoes

---
+++ ff/src/ff/seismilhoes.java
@@ -0,0 +1,48 @@
+package ff;
+
+import java.security.SecureRandom;
+
+public class seismilhoes {
+
+	public static void main(String[] args) {
+		SecureRandom randomNumbers = new SecureRandom();
+		int[] freq = new int[] { 0, 1, 2, 3, 4, 5 };
+
+		for (int roll = 1; roll <= 6000000; roll++) {
+			int face = 1 + randomNumbers.nextInt(6);
+
+			switch (face) {
+			case 1:
+				freq[0]++;
+				break;
+			case 2:
+				freq[1]++;
+				break;
+			case 3:
+				freq[2]++;
+				break;
+			case 4:
+				freq[3]++;
+				break;
+			case 5:
+				freq[4]++;
+				break;
+			case 6:
+				freq[5]++;
+				break;
+			}
+		}
+		System.out.println("Face\tFrequencia");
+		for (int i = 1; i <= 6; i++) {
+			// System.out.println("\t" + (i) + freq[ i - 1]);
+
+		}
+		System.out.println("1-\t" + freq[0]);
+		System.out.println("2-\t" + freq[1]);
+		System.out.println("3-\t" + freq[2]);
+		System.out.println("4-\t" + freq[3]);
+		System.out.println("5-\t" + freq[4]);
+		System.out.println("6-\t" + freq[5]);
+
+	}
+}
