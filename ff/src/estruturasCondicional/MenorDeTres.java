8f6f9652120797f92379b90c5c7bcfaf86d71b64
Felipe Guilherme Silva <hyperdarks693@outlook.com>
2022-10-22 07:28:49 -0300

    new

---
+++ ff/src/estruturasCondicional/MenorDeTres.java
@@ -0,0 +1,31 @@
+package estruturasCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MenorDeTres {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Primeiro valor: ");
+		int a = sc.nextInt();
+		System.out.println("Segundo valor: ");
+		int b = sc.nextInt();
+		System.out.println("Terceiro valor: ");
+		int c = sc.nextInt();
+		int menor;
+		if( a < b && a < c) {
+			menor = a;
+		}else if(b < c) {
+			menor = b;
+		}else {
+			menor = c;
+		}
+		
+		System.out.println("MENOR = "+menor);
+		
+	}
+
+}
